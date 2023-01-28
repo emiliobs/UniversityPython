4f308f89539cb8cab17b30ba4c14be8de53e6564
emilio barrera <barrera_emilio@yahoo.es>
2023-01-28 12:45:11 +0000

    Python - List Comprehension

--- loopLists.py
+++ loopLists.py
@@ -33,7 +33,76 @@ print("=======================================================================")
 
 print("=======================================================================")
 
-# i am here https://www.w3schools.com/python/python_lists_comprehension.asp
+# List Comprehension
+print("List Comprehension")
+fruits = ["apple", "banana","chery", "kiwi", "mango"]
+newList=[]
+
+for x in fruits:
+    if "a" in x:
+        newList.append(x)
+
+
+print(newList)
+
 print("=======================================================================")
+
+# just one line of code:
+print("just one line of code")
+newList = [x for x in fruits if "a" in x]
+
+print(newList)
+
+
+print("=======================================================================")
+# Condiction:
+print("COndiction")
+
+newList1 = [x for x in fruits if x != "apple"]
+print(newList1)
+
+print("=======================================================================")
+
+# with no id statement
+print("With no if statement")
+newlist2 = [x for x in fruits]
+
+print(newlist2)
+
+print("=======================================================================")
+
+# Iterable
+print("iterable")
+newList3 = [x for x in range(10)]
+print(newList3)
+
+print("=======================================================================")
+# iterable with condition
+print("Iterable with condition")
+newlist4 = [x for x in range(10) if x < 5]
+print(newlist4)
+
+print("=======================================================================")
+
+newlis5 = [x.upper() for x in fruits ]
+print(newlis5)
+
+
+print("=======================================================================")
+
+newlist6 = ["HEllo" for x in fruits]
+print(newlist6)
+print("=======================================================================")
+
+newList7 = [x if x != "banana" else "orange" for x in fruits]
+print(newList7)
+print("=======================================================================")
+print("=======================================================================")
+print("=======================================================================")
+print("=======================================================================")
+print("=======================================================================")
+print("=======================================================================")
+print("=======================================================================")
+
 
 
