65bc17e99f0b4999a1a7ce558091345661194285
emilio barrera <barrera_emilio@yahoo.es>
2023-01-29 17:56:16 +0000

    Discussion  1. Given the initial statements

---
+++ DiscussionChapter5.py
@@ -0,0 +1,38 @@
+s1 = "spam"
+s2 = "ni!"
+print(f"1. Given the initial statements of s1 = {s1} and s2 = {s2}")
+print()
+print("Show the result of evaluating each of the following string expression!")
+print("A")
+print(f"The las who say, {s2}")
+
+print("=========================================================")
+print("B")
+print(3 * s1 + 2 * s2)
+
+
+
+print("=========================================================")
+print("C")
+print(s1[1])
+
+print("=========================================================")
+print("D")
+print(s1[1:3])
+
+print("=========================================================")
+print("E")
+print(s1[2] + s2[:2])
+
+print("=========================================================")
+print("F")
+print(s1 + s2[-1])
+
+print("=========================================================")
+print("G")
+print(s1.upper())
+
+print("=========================================================")
+print("H")
+print(s2.upper().ljust(4) * 3)
+
