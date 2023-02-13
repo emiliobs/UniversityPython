1b4863af8db0694031006465571671c54ee03889
emilio barrera <barrera_emilio@yahoo.es>
2023-02-13 12:49:51 +0000

    used and called diferent fucntions

---
+++ functions/HappyBirthday.py
@@ -0,0 +1,33 @@
+
+def main():
+    singFred()
+    print()
+    singLucy()
+  
+
+def happy():
+  print("Happy birthday to you!")
+
+
+
+def singFred():
+  happy()
+  happy()
+  print("Happy birthday, dear Emilio.")
+  happy()
+  
+def singLucy():
+    happy()
+    happy()
+    print("Happy birthday, dear Lucy")
+    happy()
+    
+def sing(person):
+  happy()
+  happy()
+  print(f"Happy birthday, dear {person}")
+  happy()
+  
+  
+main() 
+  
--- functions/futvalGraph2.py
+++ functions/futvalGraph2.py
@@ -1,17 +1,13 @@
 from graphics import *
 
-
-
 def main():
   #Introduccion
   print("This program plots the growth of a 10 - year invesment. ")
-  
-  
+    
   #Get principal and interest rate
   principal = float(input("Enter the initial principal: "))
   apr = float(input("Enter the annualized interest rate: "))
-  
-  
+    
   #Create a graphics windos with labels on left edge
   win = GraphWin("Investment Growth Chart", 320, 240)
   win.setBackground("white")
@@ -21,14 +17,13 @@ def main():
   Text(Point(-1, 5000),   ' 5.0K').draw(win)
   Text(Point(-1, 7500),   ' 7.5K').draw(win)
   Text(Point(-1, 10000), ' 10.0K').draw(win)
-  
-  
+    
   #Draw bar for initial principal.
   bar =  Rectangle(Point(0, 0), Point(1, principal))
   bar.setFill("green")
   bar.setWidth(2)
   bar.draw(win)
-  
+    
   #Draw a bar for each subsequent year
   for year in range(1, 11):
     principal = principal * (1 + apr)
